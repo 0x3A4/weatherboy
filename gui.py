7b6e3634afbaa77c8b34c1b0760b71871e2c7047
tim <tsethhorton@gmail.com>
2024-01-25 09:36:18 -0600

    tab structure start

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import platform
 import sys
 import os
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt6.QtWidgets import QGridLayout, QHBoxLayout, QTabWidget, QApplication, QMainWindow, QWidget, QPushButton
 import json
 
 
@@ -15,11 +15,34 @@ app = QApplication(sys.argv)
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle('GUI TEST')
-        button = QPushButton("test")
-        self.setCentralWidget(button)
+        #initial window settings
+        self.setWindowTitle('Weatherboy')
         self.setFixedSize(QSize(600,400))
+        
+        main_layout = QGridLayout(self)
+        self.setLayout(main_layout)
+        
+        #init tab widget
+        tab = QTabWidget(self)
+
+        #1 day forecast tab
+        oneDay = QWidget(self)
+        oneDayLayout = QGridLayout()
+        
+
+        #5 day forecast tab
+        fiveDayLayout = QHBoxLayout()
+
+
+        #settings tab
+        settingsLayout = QGridLayout()
+
+
+        #tab placement
+        main_layout.addWidget(tab, 0, 3, 2, 1)
+
+
+            
 
 window = Window()
 window.show()
