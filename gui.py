5a70e9901755122f450e90a3f79334ad98b7b6fa
tim <tsethhorton@gmail.com>
2024-01-25 17:11:28 -0600

    added image capability to second tab

--- gui.py
+++ gui.py
@@ -1,7 +1,13 @@
 import platform
 import sys
 import os
-from PyQt6.QtCore import QSize, Qt
+import requests
+import tempfile
+import json
+from functools import cached_property
+from PyQt6.QtCore import QSize, Qt, QObject, pyqtSignal, QUrl
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
 from PyQt6.QtWidgets import(
     QGridLayout, 
     QHBoxLayout,
@@ -9,28 +15,66 @@ from PyQt6.QtWidgets import(
     QApplication,
     QMainWindow,
     QWidget, 
-    QPushButton
+    QLabel
     )
 
-import json
-
-
-app = QApplication(sys.argv)
 
 # import and check status code from api key via gui window. give user an error and option
 # input new api key if this is errored. also give user option to change or delete it in
 # settings. consider using hash storage of some kind
 
+
+#def urlGen(iconCode, scale):
+#    string = "https://openweathermap.org/img/wn/" + iconCode + "@" + scale + "x.png"
+#    return string
+
+
+
+
+#Image downloader. Used to pass images into widgets.
+class ImageDownloader(QObject):
+    finished = pyqtSignal(QImage)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.manager.finished.connect(self.handle_finished)
+
+    @cached_property
+    def manager(self):
+        return QNetworkAccessManager()
+
+    def start_download(self, url):
+        self.manager.get(QNetworkRequest(url))
+
+    def handle_finished(self, reply):
+        if reply.error() == QNetworkReply.error:
+            print("error: ", reply.errorString())
+            return
+        image = QImage()
+        image.loadFromData(reply.readAll())
+        self.finished.emit(image)
+
+
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+
         #initial window settings
         self.setWindowTitle('Weatherboy')
-        self.setFixedSize(QSize(600,400))
-        
+        self.setFixedSize(QSize(640,480))
         mainLayout = QGridLayout(self)
         self.setLayout(mainLayout)
         mainLayout.setSpacing(20)
+
+
+        #downloader
+        self.downloader = ImageDownloader()
+
+        self.downloader.finished.connect(self.handle_finished)
+        self.download()
+        
         #init tab widget
         tabs = QTabWidget(self)
 
@@ -38,12 +82,44 @@ class Window(QMainWindow):
         oneDay = QWidget(self)
         oneDayLayout = QGridLayout()
         oneDay.setLayout(oneDayLayout)
-        
+
 
         #5 day forecast tab
         fiveDay = QWidget(self)
-        fiveDayLayout = QHBoxLayout()
+        fiveDayLayout = QHBoxLayout(self)
         fiveDay.setLayout(fiveDayLayout)
+        
+        #init days for 5 day forecast
+        #dayOne   = QWidget(self)
+        #dayTwo   = QWidget(self)
+        #dayThree = QWidget(self)
+        #dayFour  = QWidget(self)
+        #dayFive  = QWidget(self)
+
+        #grab images as icons for each day
+        self.dayOnePNG   = QLabel(alignment=Qt.AlignmentFlag.AlignCenter)
+        #self.dayTwoPNG   = QLabel(alignment=Qt.AlignmentFlag.AlignJustify)
+        #self.dayThreePNG = QLabel(alignment=Qt.AlignmentFlag.AlignJustify)
+        #self.dayFourPNG  = QLabel(alignment=Qt.AlignmentFlag.AlignJustify)
+        #self.dayFivePNG  = QLabel(alignment=Qt.AlignmentFlag.AlignJustify)
+        
+        #self.dayOnePNG.resize(120, 120)
+        #self.dayTwoPNG.resize(120, 120)
+        #self.dayThreePNG.resize(120, 120)
+        #self.dayFourPNG.resize(120, 120)
+        #self.dayFivePNG.resize(120, 120)
+
+        #self.dayOnePNG.setStyleSheet('border:1px solid black;')
+        #self.dayTwoPNG.setStyleSheet('border:1px solid black;')
+        #self.dayThreePNG.setStyleSheet('border:1px solid black;')
+        #self.dayFourPNG.setStyleSheet('border:1px solid black;')
+        #self.dayFivePNG.setStyleSheet('border:1px solid black;')\
+        
+
+
+        #place days 1-5
+        fiveDayLayout.addWidget(self.dayOnePNG)
+        
 
         #settings tab
         settings = QWidget(self)
@@ -52,21 +128,29 @@ class Window(QMainWindow):
 
         #add the three tabs
         tabs.addTab(oneDay, "Today")
-        tabs.addTab(fiveDay, "5 day forecast")
+        tabs.addTab(fiveDay, "5 Day Forecast")
         tabs.addTab(settings, "Settings")
 
         #tab placement
         tabs.setTabPosition(QTabWidget.TabPosition.North)
         self.setCentralWidget(tabs)
         tabs.setMovable(False)
-        #mainLayout.addWidget(tabs, 1, 2, alignment=Qt.AlignmentFlag.AlignCenter)
+
         
+        #mainLayout.addWidget(tabs, 1, 2, alignment=Qt.AlignmentFlag.AlignCenter)
+    def handle_finished(self, image):
+        pixmap = QPixmap.fromImage(image)
+        self.dayOnePNG.setPixmap(pixmap)
+
+    def download(self):
+        url = QUrl('http://openweathermap.org/img/wn/01d@2x.png')
+        self.downloader.start_download(url)
+         
 
 
-            
-
-window = Window()
-window.show()
+app = QApplication(sys.argv)
+w = Window()
+w.show()
 
 app.exec()
 
